e49836fdb854470ec678981a61b8c680b2180e1d
Prachi0998 <prachisingh0998@gmail.com>
2021-02-16 11:40:01 +0530

    new features added to features1

---
+++ newcode1.py
@@ -0,0 +1,8 @@
+print("Division")
+a=int(input("Enter the number"))
+b=int(input("Enter the number"))
+if a>b:
+	c=a\b
+else:
+	c=b\a
+print("Division:",c)
---
+++ third.py
@@ -0,0 +1,5 @@
+print("Sumation=")
+a=int(input("Enter the number")
+b=int(input("Enter the number")
+c=a+b
+print("Add:",c)
